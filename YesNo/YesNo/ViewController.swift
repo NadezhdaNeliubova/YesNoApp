ce94d495a41ac8193339e88c26531cd989ca6fde
Nadia Neliubova <nadeika.spb@gmail.com>
2023-07-09 16:08:15 +0800

    repeated the task from the video

--- YesNo/YesNo/ViewController.swift
+++ YesNo/YesNo/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var answerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        answerButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func answerButtonAction(_ sender: Any) {
+        let answer = Bool.random()
+        answerLabel.text = ""
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.answerLabel.text = answer ? "YES" : "NO"
+        }
+        
+    }
+    
 }
 
